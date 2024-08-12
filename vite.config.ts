91a9a027a384cc534f445bcc1dd349b04549a334
= <=>
2024-08-12 20:21:52 +0530

    implemented: the basic setup for the storybook docs

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "StorybookDocs",
+      fileName: "storybook-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+  },
+  plugins: [react()],
+});
